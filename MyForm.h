1060cfecdff386f796abf9befd9dcb6f8034dba2
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2023-10-02 12:55:02 +0300

    add color form labeks and button and textboks

--- MyForm.h
+++ MyForm.h
@@ -45,6 +45,7 @@
 			private: System::Windows::Forms::TextBox^ textBox4;
 			private: System::Windows::Forms::Label^ label4;
 			private: System::Windows::Forms::Button^ button1;
+
 			protected:
 
 			private:
@@ -74,54 +75,60 @@
 					// label1
 					// 
 					this->label1->AutoSize = true;
+					this->label1->BackColor = System::Drawing::Color::DodgerBlue;
+					this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, System::Drawing::FontStyle::Bold));
 					this->label1->Location = System::Drawing::Point(12, 9);
 					this->label1->Name = L"label1";
-					this->label1->Size = System::Drawing::Size(137, 16);
+					this->label1->Size = System::Drawing::Size(221, 25);
 					this->label1->TabIndex = 0;
 					this->label1->Text = L"Введіть значення Х:";
 					// 
 					// label2
 					// 
 					this->label2->AutoSize = true;
+					this->label2->BackColor = System::Drawing::Color::DodgerBlue;
+					this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, System::Drawing::FontStyle::Bold));
 					this->label2->Location = System::Drawing::Point(11, 47);
 					this->label2->Name = L"label2";
-					this->label2->Size = System::Drawing::Size(138, 16);
+					this->label2->Size = System::Drawing::Size(219, 25);
 					this->label2->TabIndex = 1;
 					this->label2->Text = L"Введіть значення Y:";
 					// 
 					// label3
 					// 
 					this->label3->AutoSize = true;
+					this->label3->BackColor = System::Drawing::Color::DodgerBlue;
+					this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, System::Drawing::FontStyle::Bold));
 					this->label3->Location = System::Drawing::Point(12, 88);
 					this->label3->Name = L"label3";
-					this->label3->Size = System::Drawing::Size(137, 16);
+					this->label3->Size = System::Drawing::Size(220, 25);
 					this->label3->TabIndex = 2;
 					this->label3->Text = L"Введіть значення Z:";
 					// 
 					// textBox1
 					// 
-					this->textBox1->Location = System::Drawing::Point(182, 13);
+					this->textBox1->Location = System::Drawing::Point(237, 13);
 					this->textBox1->Name = L"textBox1";
-					this->textBox1->Size = System::Drawing::Size(203, 22);
+					this->textBox1->Size = System::Drawing::Size(169, 22);
 					this->textBox1->TabIndex = 3;
 					// 
 					// textBox2
 					// 
-					this->textBox2->Location = System::Drawing::Point(182, 47);
+					this->textBox2->Location = System::Drawing::Point(237, 47);
 					this->textBox2->Name = L"textBox2";
-					this->textBox2->Size = System::Drawing::Size(203, 22);
+					this->textBox2->Size = System::Drawing::Size(169, 22);
 					this->textBox2->TabIndex = 4;
 					// 
 					// textBox3
 					// 
-					this->textBox3->Location = System::Drawing::Point(182, 88);
+					this->textBox3->Location = System::Drawing::Point(238, 92);
 					this->textBox3->Name = L"textBox3";
-					this->textBox3->Size = System::Drawing::Size(203, 22);
+					this->textBox3->Size = System::Drawing::Size(168, 22);
 					this->textBox3->TabIndex = 5;
 					// 
 					// textBox4
 					// 
-					this->textBox4->Location = System::Drawing::Point(15, 163);
+					this->textBox4->Location = System::Drawing::Point(12, 213);
 					this->textBox4->Multiline = true;
 					this->textBox4->Name = L"textBox4";
 					this->textBox4->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
@@ -131,28 +138,35 @@
 					// label4
 					// 
 					this->label4->AutoSize = true;
-					this->label4->Location = System::Drawing::Point(12, 144);
+					this->label4->BackColor = System::Drawing::Color::DodgerBlue;
+					this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+						static_cast<System::Byte>(204)));
+					this->label4->Location = System::Drawing::Point(12, 185);
 					this->label4->Name = L"label4";
-					this->label4->Size = System::Drawing::Size(219, 16);
+					this->label4->Size = System::Drawing::Size(349, 25);
 					this->label4->TabIndex = 7;
 					this->label4->Text = L"Рузультат виконання програми:";
 					// 
 					// button1
 					// 
 					this->button1->AllowDrop = true;
-					this->button1->Location = System::Drawing::Point(299, 312);
+					this->button1->BackColor = System::Drawing::Color::DodgerBlue;
+					this->button1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+						static_cast<System::Byte>(0)));
+					this->button1->Location = System::Drawing::Point(307, 352);
 					this->button1->Name = L"button1";
-					this->button1->Size = System::Drawing::Size(86, 23);
+					this->button1->Size = System::Drawing::Size(118, 33);
 					this->button1->TabIndex = 8;
 					this->button1->Text = L"Виконати";
-					this->button1->UseVisualStyleBackColor = true;
+					this->button1->UseVisualStyleBackColor = false;
 					this->button1->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::button1_MouseClick);
 					// 
 					// MyForm
 					// 
 					this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 					this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-					this->ClientSize = System::Drawing::Size(403, 347);
+					this->BackColor = System::Drawing::Color::Yellow;
+					this->ClientSize = System::Drawing::Size(437, 397);
 					this->Controls->Add(this->button1);
 					this->Controls->Add(this->label4);
 					this->Controls->Add(this->textBox4);
@@ -170,33 +184,35 @@
 				}
 #pragma endregion
 			private: System::Void button1_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-				//конвертую текст в дабл
+				textBox4->Text = "";// чистим вікно перед кожним новим виводом 
+				//конвертую текст в число
 				double x = Convert::ToDouble(textBox1->Text); 
 				double y = Convert::ToDouble(textBox2->Text);
 				double z = Convert::ToDouble(textBox3->Text);
-				//вивожу в текстбокс інфу про данні введені користувачем 
-				textBox4->Text += " Лаб. роб. №1. ст.гр. РТ-23б Онищук.В.О.";
+				//вивожу у вікно інформацію про данні введені користувачем 
+				textBox4->Text += " Лаб. роб. №1. ст.гр. РТ-23б Харжевський О.О";
 				textBox4->Text += "\r\n\n X = " + Convert::ToString(x);
 				textBox4->Text += "\r\n\n\n"" Y = " + Convert::ToString(y);
 				textBox4->Text += "\r\n\n\n\n"" Z = " + Convert::ToString(z);
 			
 				
-				const double pinashist = 2.61799;
+				const double pi = 0.523;
 				double denominator = 0;
 				double numerator = 0;
 				double sum = 0;
 				float result = 0;
 
-				//розклав вираз на дії і обчислив все окремо
+				//обчислення по діях
 				sum = pow(2, pow(y, x))+ pow(3, x * y);
-				numerator = y * (atan(z) - pinashist);
+				numerator = y * (atan(z) - pi);
 				denominator = fabs(x) + (1 / (pow(y, 2) + 1));
 
 				result = sum - numerator / denominator;
-				//вивід кінцевого результату в текстбокс
+				//вивід кінцевого результату у вікно
 				textBox4->Text += "\r\n\n\n\n\n"" Result = " + Convert::ToString(result);
 
 					
 			}
-	};
+	
+};
 }
